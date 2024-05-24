9d1a331b5ffc06e1aacec23fe8d1621d17aca36f
Shahe Lecini <110564512+lech1n@users.noreply.github.com>
2024-05-25 00:24:22 +0200

    Add files via upload

---
+++ part1/courseInformation/src/Content.jsx
@@ -0,0 +1,15 @@
+const Part = ({part , exercises}) =>{
+    return <p> {part} {exercises} </p>
+}
+
+const Content = ({course}) => {
+  return (
+    <>
+      <Part part={course.parts[0].name} exercises={course.parts[0].exercises}/>
+      <Part part={course.parts[1].name} exercises={course.parts[1].exercises}/> 
+      <Part part={course.parts[2].name} exercises={course.parts[2].exercises}/> 
+    </>
+  )
+}
+
+export default Content
